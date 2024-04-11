f6617720617adf7fceaa0e96ff17b9eb3b4008b2
ptthao0801 <ptthao0801@gmail.com>
2024-04-11 16:48:50 +0700

    change

---
+++ src/bai1.sql
@@ -0,0 +1,17 @@
+create database demo;
+
+use demo;
+
+create table Student(
+                        id int,
+                        name varchar(200),
+                        age int,
+                        country varchar(50)
+);
+
+create table Teacher(
+                        id int,
+                        name varchar(200),
+                        age int,
+                        country varchar(100)
+);
